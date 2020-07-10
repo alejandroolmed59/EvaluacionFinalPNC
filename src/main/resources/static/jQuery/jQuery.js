ecdd326dc2830524531f2158dd7e41309efd699d
Cesar Rosales <00060917@uca.edu.sv>
2020-07-10 00:42:15 -0600

    fail

--- src/main/resources/static/jQuery/jQuery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#yearPicker').datepicker({
-        changeMonth: false,
-        changeYear: true,
-        showButtonPanel: true,
-        yearRange: '2005:2020', // Optional Year Range
-        dateFormat: 'yy',
-        });
-});
